657e233e23376b0e813ffd318d70dfe09e457ad8
bakulya-1 <baktygul.bishkek@icloud.com>
2024-12-11 23:29:50 +0600

    add: requirements txt

--- classwork/classw4.py
+++ classwork/classw4.py
@@ -1,11 +1,31 @@
 #Тема №4. Закрепление пройденного материала - Использование ООП в проекте
 """ Модуль, пакеты - библиотека. Декораторы."""
 
+
+# import lessons.lesson4  as mymodule
+# from lessons import lesson4 as mymodule
+
+# import random
+# import math
+# from json import JSONDecodeError
+
+
+# mymodule.greet('Kirtito')
+# print(mymodule.TEST)
+
 """""
 Модуль и Пакеты
 Встроенные модули и библиотеки
 Внешние пакеты, модули и библиотеки
 """""
 
+#from colorama import init, Fore, Style
+
+#init()
+
+#print(Fore.RED + "This text is red!")
+#print(Fore.GREEN + "This text is green!")
+
+#from requests import get, post, put, delete
 
 
