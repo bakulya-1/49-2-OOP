78364dde6a6b9097ab8e3fd81df47784e4a51f4f
bakulya-1 <baktygul.bishkek@icloud.com>
2024-12-13 19:58:29 +0600

    homework: added hw4

--- homework/hw4.py
+++ homework/hw4.py
@@ -5,4 +5,5 @@ import requests  #импортируем библиотеку requests
 response = requests.get("https://api.github.com") # отправляем запрос на github
 
 print(response.json())
-#выводит ответ, который вернет github, информация о сервере
+#выводит ответ, который вернет на github, информация о сервере
+
